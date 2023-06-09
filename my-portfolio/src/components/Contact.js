410b619c2fe3f347bc946b44b2d9f3878b1d6e16
aporria <aporria0451@gmail.com>
2023-06-08 19:01:13 -0700

    deploying app

--- my-portfolio/src/components/Contact.js
+++ my-portfolio/src/components/Contact.js
@@ -69,11 +69,10 @@ export default function Contact() {
           name="contact"
           className="lg:w-1/3 md:w-1/2 flex flex-col md:ml-auto w-full md:py-8 mt-8 md:mt-0">
           <h2 className="text-white sm:text-4xl text-3xl mb-1 font-medium title-font">
-            Hire Me
+            Work With Me
           </h2>
           <p className="leading-relaxed mb-5">
-            Lorem ipsum dolor sit amet consectetu, adipisicing elit. Illum
-            suscipit officia aspernatur veritatis. Asperiores, aliquid?
+            If you're interested in working with me, send me a message below!
           </p>
           <div className="relative mb-4">
             <label htmlFor="name" className="leading-7 text-sm text-gray-400">
